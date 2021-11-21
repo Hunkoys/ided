f7587756abcbdabbb89338e498d45a1e39f20cf2
Dominic Victoria <hunkyminik@gmail.com>
2021-11-20 21:28:03 -0800

    added ids and indices HOF

--- test/tools.ts
+++ test/tools.ts
@@ -1,4 +1,5 @@
 import { Element } from '../src/Element';
+import { Index } from '../src/types';
 
 export const constructors = {
   string: String,
@@ -14,6 +15,15 @@ export function any(type: string) {
 export function values(element: Element | null) {
   if (element == null) fail(`value element is invalid: ${element}`);
   return element.value;
+}
+
+export function ids(element: Element | null) {
+  if (element == null) fail(`value element is invalid: ${element}`);
+  return element.id;
+}
+
+export function indices(_: Element | null, i: Index) {
+  return i;
 }
 
 export function valueOf(element: Element | null): any {
